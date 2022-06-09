31a2e80f1ad8dd7f0a921ba450c2a02b11b87bee
bryanstitt <bryanstitt99@gmail.com>
2022-06-08 22:28:24 -0500

    last commit

--- main.py
+++ main.py
@@ -6,25 +6,24 @@ from get_image import get_image
 def drop():
     keyboard.write('kd')
     keyboard.send('enter')
-    time.sleep(5)
+    time.sleep(8.108)
     get_image()
     index, ed = get_best_position()
+    time.sleep(5)
+
     match index:
         case 0:
-            time.sleep(.25)
             keyboard.write('+:one:')
         case 1:
-            time.sleep(.25)
             keyboard.write('+:two:')
         case 2:
-            time.sleep(.25)
             keyboard.write('+:three:')
         case 3:
-            time.sleep(.25)
             keyboard.write('+:four:')
 
     keyboard.send('enter')
     time.sleep(1)
+    
     if ed == 4:
         keyboard.write('klu')
         keyboard.send('enter')
